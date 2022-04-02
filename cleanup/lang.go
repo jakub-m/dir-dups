50ca8453db8152ce06cfa434c5b2346b24cfe9ab
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2022-04-02 17:40:00 +0200

    wip

--- cleanup/lang.go
+++ cleanup/lang.go
@@ -4,12 +4,30 @@ import (
 	"bufio"
 	"greasytoad/cleanup/parser"
 	"io"
+	"io/ioutil"
 	"strings"
 )
 
-func ProcessManifest(minilang io.Reader, input io.Reader) (string, error) {
-	_, err := parserMinilang(minilang)
-	return "", err
+func ProcessManifest(minilangReader io.Reader, inputReader io.Reader) (string, error) {
+	mil, err := parserMinilang(minilangReader)
+	inputBytes, err := ioutil.ReadAll(inputReader)
+	if err != nil {
+		return "", err
+	}
+	input := string(inputBytes)
+
+	_ = mil
+	_ = input
+	return "", nil
+	// hashGroups := collectHashGroups(input)
+
+	// The processing is as follows:
+	// - Read the input lines and collect the lines into "hash groups". A hash group is a set of lines from the input manifest with the same hash.
+	// - Infer new actions per hash group.
+	// - Again iterate through input lines, and modity them accorting to the actions inferred in the previous step.
+	//
+	// Inferring actions per hash group works as follows:
+	// - check if hashgroup matches all the expressions. If so, apply the mapping.
 }
 
 func parserMinilang(r io.Reader) (minilangInstruction, error) {
@@ -35,6 +53,26 @@ func parserMinilang(r io.Reader) (minilangInstruction, error) {
 }
 
 type minilangInstruction []parser.InstructionNode
+
+// func (mi minilangInstruction) process(r io.Reader) (string, error) {
+// 	out := &strings.Builder{}
+// 	scanner := bufio.NewScanner(r)
+// 	for scanner.Scan() {
+// 		line := scanner.Text()
+// 		if strings.HasPrefix(line, "#") {
+// 			fmt.Fprintf(out, "%s\n", line)
+// 			continue
+// 		}
+// 		// // HERE duplicated logic
+// 		// parts := strings.Split(line, "\t")
+// 		// lineAction, lineHash, linePath := parts[0], parts[1], parts[2]
+// 		// lineko
+// 	}
+// 	if err := scanner.Err(); err != nil {
+// 		return "", err
+// 	}
+// 	return out.String(), nil
+// }
 
 func strip(s string) string {
 	s = strings.Trim(s, " \n\t")
